400cb9fb8b4177a8bf6a136994a16f57146deef3
Brian <brian.mwangi2@student.moringaschool.com>
2024-06-03 06:59:04 +0300

    passes

---
+++ lib/absolute_package/package1/module2.py
@@ -0,0 +1,3 @@
+from package1.module1 import function1
+function1
+# Absolute import as we have the accurate path of where the import is coming from 
--- lib/absolute_package/package2/subpackage1/module6.py
+++ lib/absolute_package/package2/subpackage1/module6.py
@@ -1,2 +1,5 @@
 def function1():
     print('Function 1 in module 6')
+
+from package2.subpackage1.module6 import function1
+function1()
